2ef3e47b878fcc62f47c1268ac4bc2d9aa28c5e8
Fernando Carta <cartafernando@outlook.com>
2024-08-11 10:55:59 -0500

    Inicializacion repo Backend TorneoPlaya

---
+++ src/main/java/com/torneo_playa/TorneoPlayaApplication.java
@@ -0,0 +1,13 @@
+package com.torneo_playa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneoPlayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorneoPlayaApplication.class, args);
+	}
+
+}
